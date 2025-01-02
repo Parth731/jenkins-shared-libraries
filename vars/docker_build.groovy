2399c8a16f80bed430dd7328a5b5d090c30fdb23
Parth Patel <62897911+Parth731@users.noreply.github.com>
2025-01-02 19:56:36 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,5 @@
 def call(String ProjectName, String ImageTag, String DockerHubUser){
+  echo 'docker build start'
   sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+  echo 'docker build end'
 }
