a196c89b87b0a7db619667cedf62252db68b40cf
Parth Patel <62897911+Parth731@users.noreply.github.com>
2025-01-02 20:56:44 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,6 +1,3 @@
-// vars/dockerBuild.groovy
-def call(String imageName, String tag, String dockerHubUser) {
-    echo "Building Docker image: ${dockerHubUser}/${imageName}:${tag}"
-    sh "docker build -t ${imageName}:${tag} ."
-    sh "docker tag ${imageName}:${tag} ${dockerHubUser}/${imageName}:${tag}"
+def call(String ProjectName, String ImageTag, String DockerHubUser){
+  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
 }
