f125727290084be7668f587125868e9e7612f40d
Parth Patel <62897911+Parth731@users.noreply.github.com>
2025-01-02 21:19:07 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,3 +1,5 @@
-def call(String url, String branch){
-  git url: url, branch: branch
+def call(String repoUrl, String branch) {
+  echo "Cloning the repository: ${repoUrl}, Branch: ${branch}"
+  git url: repoUrl, branch: branch
+  echo 'Code cloned successfully!'
 }
